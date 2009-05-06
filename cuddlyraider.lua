dd0612fcaadee347e722dc4cd0d9afa013cf5522
Trond A Ekseth <troeks@gmail.com>
2009-05-06 13:24:13 +0200

    Initial commit.

---
+++ cuddlyraider.lua
@@ -0,0 +1,87 @@
+local _VERSION = GetAddOnMetadata('CuddlyRaider', 'version')
+local function argcheck(value, num, ...)
+	assert(type(num) == 'number', "Bad argument #2 to 'argcheck' (number expected, got "..type(num)..")")
+
+	for i=1,select("#", ...) do
+		if type(value) == select(i, ...) then return end
+	end
+
+	local types = strjoin(", ", ...)
+	local name = string.match(debugstack(2,2,0), ": in function [`<](.-)['>]")
+	error(("Bad argument #%d to '%s' (%s expected, got %s"):format(num, name, types, type(value)), 3)
+end
+
+local print = function(...) print("|cff33ff99Cuddly Raider:|r ", ...) end
+local error = function(...) print("|cffff0000Error:|r "..string.format(...)) end
+
+local event_metatable = {
+	__call = function(funcs, self, ...)
+		for _, func in ipairs(funcs) do
+			func(self, ...)
+		end
+	end,
+}
+
+local cr = CreateFrame('Frame', 'CuddlyRaider', UIParent)
+
+--[[ Event API ]]
+
+local RegisterEvent = cr.RegisterEvent
+function cr:RegisterEvent(event, func)
+	argcheck(event, 2, 'string')
+
+	if(type(func) == 'string' and type(self[func]) == 'function') then
+		func = self[func]
+	end
+
+	local curev = self[event]
+	if(curev and func) then
+		if(type(curev) == 'function') then
+			self[event] = setmetatable({curev, func}, event_metatable)
+		else
+			for _, infunc in ipairs(curev) do
+				if(infunc == func) then return end
+			end
+
+			table.insert(curev, func)
+		end
+	elseif(self:IsEventRegistered(event)) then
+		return
+	else
+		if(func) then
+			self[event] = func
+		elseif(not self[event]) then
+			error("Handler for event [%s] does not exist.", event)
+		end
+
+		RegisterEvent(self, event)
+	end
+end
+
+local UnregisterEvent = cr.UnregisterEvent
+function cr:UnregisterEvent(event, func)
+	argcheck(event, 2, 'string')
+
+	local curev = self[event]
+	if(type(curev) == 'table' and func) then
+		for k, infunc in ipairs(curev) do
+			if(infunc == func) then
+				curev[k] = nil
+
+				if(#curev == 0) then
+					table.remove(curev, k)
+					UnregisterEvent(self, event)
+				end
+			end
+		end
+	else
+		self[event] = nil
+		UnregisterEvent(self, event)
+	end
+end
+
+cr:SetScript('OnEvent', function(self, event, ...)
+	self[event](self, event, ...)
+end)
+
+cr.version = _VERSION
